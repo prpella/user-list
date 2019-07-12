6e0934d0967a8dad6f9e3c1f7a5a4906698723dc
Ela Blechschmidt <ela@posteo.de>
2019-07-12 10:29:27 +0200

    set up react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render () {
+    return (
+      <main className="main">
+        <UserList />
+      </main>
+    )
+  }
 }
 
-export default App;
+const UserList = () => {
+
+  return (
+    <ul>
+      <li>
+        <User />
+      </li>
+    </ul>
+  )
+}
+
+const User = () => {
+  return (
+    <div>
+      <li>Headline</li>
+      <li>UserAame</li>
+      <li>UserAge</li>
+      <li>Location</li>
+      <li>Last Login</li>
+    </div>
+  )
+}
+
+export default App
